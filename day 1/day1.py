eebc01d1120a66a127473225a83a9352d8dd1c23
1r21-Eny <e.trapani.uk@gmail.com>
2025-10-17 00:31:30 +0100

    did bonus and cleaned up prints

--- day 1/day1.py
+++ day 1/day1.py
@@ -6,12 +6,14 @@ def counting(string):
     constonants = "bcdfghjklmnpqrstvwxyz"
     vowels = "aeiou"
     digits = "0123456789"
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     
     VowelCount = 0
     SpaceCount = 0
     ConstonantCount = 0
     DigitCount = 0
     SpaceCount = string.count(" ")
+    IsPangram = True
     
     for letter in string:
         if letter in vowels:
@@ -21,6 +23,15 @@ def counting(string):
         elif letter in digits:
             DigitCount += 1
 
-    return VowelCount, ConstonantCount, DigitCount, SpaceCount
-VowelCount, ConstonantCount, DigitCount, SpaceCount = counting(string)
-print(f" VowelCount: {VowelCount}\n ConstonantCount: {ConstonantCount}\n DigitCount: {DigitCount}\n SpaceCount: {SpaceCount}\n")
+    for letter in alphabet:
+        if letter not in string:
+            IsPangram = False
+            break
+        
+    return VowelCount, ConstonantCount, DigitCount, SpaceCount, IsPangram
+VowelCount, ConstonantCount, DigitCount, SpaceCount, IsPangram = counting(string)
+print(f" VowelCount: {VowelCount}")
+print(f"ConstonantCount: {ConstonantCount}")
+print(f"DigitCount: {DigitCount}")
+print(f"SpaceCount: {SpaceCount}")
+print(F"Is a pangram: {IsPangram}")
