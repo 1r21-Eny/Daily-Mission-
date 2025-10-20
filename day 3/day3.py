ed429f08079db79428ad115990873219f5937116
1r21-Eny <e.trapani.uk@gmail.com>
2025-10-20 17:16:01 +0100

    day 3 started

---
+++ day 3/day3.py
@@ -0,0 +1,8 @@
+Ans = None
+while True:
+    while True:
+        Decision = int(input("Addition (1), Subtraction (2), Multiplication (3), Division (4), Exit (5): "))
+        if Decision ==  1 or Decision == 2 or Decision == 3 or Decision == 4 or Decision ==5:
+            break
+        else:
+            print("invalid option try again")
