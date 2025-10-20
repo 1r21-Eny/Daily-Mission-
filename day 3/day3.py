291d7f7334a2ef683cb1fce4c42accf836a63bdb
1r21-Eny <e.trapani.uk@gmail.com>
2025-10-20 17:21:28 +0100

    added addition

--- day 3/day3.py
+++ day 3/day3.py
@@ -1,4 +1,16 @@
 Ans = None
+
+def addition():
+    Nums = []
+    while True:
+        TempNum = input("Enter number to add or (EXIT) to stop adding numbers: ")
+        if TempNum == "EXIT":
+            break
+        else:
+            TempNum = int(TempNum)
+            Nums.append(TempNum)
+    Ans = sum(Nums)
+    print(f"sum of: {Nums} is: {Ans}")
 while True:
     while True:
         Decision = int(input("Addition (1), Subtraction (2), Multiplication (3), Division (4), Exit (5): "))
@@ -6,3 +18,8 @@ while True:
             break
         else:
             print("invalid option try again")
+    if Decision == 5:
+        print("bye")
+        break
+    else:
+        addition()
